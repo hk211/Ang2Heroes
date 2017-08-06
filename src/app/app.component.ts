614cbef79e0f9455c321c70873a83986b9091e2c
HAKAN\Hakz <hakankaraagac@gmail.com>
2017-08-06 20:04:41 +0100

    Added array of heroes a loop in the template to display them all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,13 @@ export class Hero {
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
+
+  <ul class="heroes">
+  	<li *ngFor="let hero of heroes">
+  		<span class="badge">{{hero.id}}</span> {{hero.name}}
+  	</li>
+  </ul>
+
   <h2>{{hero.name}}</h2>
   <div><label>id: </label>{{hero.id}}</div>
   <div>
@@ -20,7 +27,22 @@ export class Hero {
 export class AppComponent  { 
 	name = 'Angular'; 
 	title = 'Tour of Heroes';
+	heroes = HEROES;
   	hero: Hero = {
   		id: 1,
   		name: 'Windstorm'
+  	}
 };
+
+const HEROES: Hero[] = [
+	{ id: 11, name: 'Mr. Nice' },
+	{ id: 12, name: 'Narco' },
+  	{ id: 13, name: 'Bombasto' },
+  	{ id: 14, name: 'Celeritas' },
+  	{ id: 15, name: 'Magneta' },
+  	{ id: 16, name: 'RubberMan' },
+  	{ id: 17, name: 'Dynama' },
+  	{ id: 18, name: 'Dr IQ' },
+  	{ id: 19, name: 'Magma' },
+  	{ id: 20, name: 'Tornado' }
+];
